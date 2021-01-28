f887683f5f5688c27fc45b6863943f9f4d63d49c
HGW <45515388+HongGunWoo@users.noreply.github.com>
2021-01-29 03:49:17 +0900

    Delete rotate_matrix_by_90_degree.py

--- rotate_matrix_by_90_degree.py
+++
@@ -1,9 +0,0 @@
-#2차원 리스트 90도 회전
-def rotate_a_matrix_by_90_degree(a):
-    n=len(a)
-    m=len(a[0])
-    result=[[0]*n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n-i-1]=a[i][j]
-    return result
